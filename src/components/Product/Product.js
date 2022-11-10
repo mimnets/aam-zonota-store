62f48b10dba34df182a70c38c514a54f85b8f5c2
Monirul Islam <mdmonirulislammonir@gmail.com>
2022-11-10 19:49:52 +0600

    Add to Cart button is working

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ('./Product.css');
 
 const Product = ({product}) => {
-    const {images, title, description, brand, price, rating} = product;
+    const {images, title, description, brand, price, rating, id} = product;
+
+    const addToCart = (id) =>{
+        console.log('button clicked', id)
+    }
     return (
         <div className='product'>
             <img src={images[0]} alt=""/>
@@ -12,7 +16,7 @@ const Product = ({product}) => {
             <p>Brand: {brand}</p>
             <p>Rating: {rating}</p>
             <div>
-                <button>Add to Cart</button>
+                <button onClick={() => addToCart(id)}>Add to Cart</button>
             </div>
         </div>
     );
